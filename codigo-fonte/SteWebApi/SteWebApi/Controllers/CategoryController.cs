9bd7bcd5436af72b99516fb3215bb71b781626d7
Brendon Guimarães <brendonguima@gmail.com>
2024-09-19 06:11:14 -0300

    concluida as alteracoes

---
+++ codigo-fonte/SteWebApi/SteWebApi/Controllers/CategoryController.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using SteWebApi.Model;
+
+namespace SteWebApi.Controllers;
+
+[Authorize]
+[Route("api/[controller]")]
+[ApiController]
+
+public class CategoryController : ControllerBase
+{
+    private readonly MongoDbContext _MongoDbContext;
+
+    public CategoryController(MongoDbContext context)
+    {
+        _MongoDbContext = context;
+    }
+
+    [HttpPost("Create")]
+    public async Task<ActionResult> Create(Category model)
+    {
+        await _MongoDbContext.Category.InsertOneAsync(model);
+        return Ok(model);
+    }
+
+    [HttpPut("Edit/{id}")]
+    public async Task<ActionResult> UpdateCategoryName(string id, [FromBody] Category newCategory)
+    {
+        var category = await _MongoDbContext.Category.Find(c => c.Id == id).FirstOrDefaultAsync();
+        if (category == null) return NotFound();
+        category.Name = newCategory.Name;
+        await _MongoDbContext.Category.ReplaceOneAsync(c => c.Id == id, category);
+        return NoContent();
+    }
+
+    [HttpDelete("Delete/{id}")]
+    public async Task<ActionResult> Delete(string id)
+    {
+        //add
+        var category = await _MongoDbContext.Category.FindOneAndDeleteAsync(x => x.Id == id);
+        if (category == null) NotFound();
+        return NoContent();
+    }
+
+    [HttpGet("Get/{id}")]
+    public async Task<ActionResult> GetById(string id)
+    {
+        var category = await _MongoDbContext.Category.Find(c => c.Id == id).FirstOrDefaultAsync();
+        if (category == null) return NotFound();
+
+        //add
+        
+        return Ok(category);
+    }
+
+    [HttpGet("GetAll")]
+    public async Task<ActionResult> GetAll()
+    {
+        var model = await _MongoDbContext.Category.Find(_ => true).ToListAsync();
+        return Ok(model);
+    }
+}
--- codigo-fonte/SteWebApi/SteWebApi/MongoDbContext.cs
+++ codigo-fonte/SteWebApi/SteWebApi/MongoDbContext.cs
@@ -6,8 +6,10 @@ namespace SteWebApi;
 public class MongoDbContext
 {
     private readonly IMongoDatabase _database;
-    public IMongoCollection<User> Users => _database.GetCollection<User>("Usuarios");
-
+    public IMongoCollection<User> Users => _database.GetCollection<User>("User");
+    public IMongoCollection<Category> Category => _database.GetCollection<Category>("Category");
+    
+    
     public MongoDbContext(IConfiguration configuration)
     {
         var connectionString = configuration.GetConnectionString("MongoDB");
